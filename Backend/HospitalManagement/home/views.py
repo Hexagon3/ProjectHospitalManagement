886a1301f34b5146c001dd9b321522e1f4521eca
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2022-09-01 10:33:45 +0530

    Django Setup and init

---
+++ Backend/HospitalManagement/home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(req):
+    return HttpResponse(
+        """
+        <h1>Project : Hospital Management System </h1>
+        <h2> Sub Project : Nurse Scheduling in bed </h2>
+        <p> Design the ui of home page and then it will looks better </p>
+        """
+    )
+    
