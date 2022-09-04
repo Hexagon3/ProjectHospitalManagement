43f2b0ad05c2de188e1dad0fbfc1aa656c0d4f80
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2022-09-04 20:02:44 +0530

    None

--- Backend/HospitalManagement/home/models.py
+++ Backend/HospitalManagement/home/models.py
@@ -7,4 +7,12 @@ class Nurse (models.Model):
     def __init__(self: _Self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.name = models.CharField(max_length=20)
+        self.profile_pic = models.ImageField()
+        self.bio = models.TextField()
+        self.email = models.EmailField()
+        self.address = models.TextField()
+        self.license = models.CharField(max_length=12)
+        self.contact_no = models.CharField(max_length=10)
         self.cost = 0
+        self.allocated_word = None
+        self.shift = None
