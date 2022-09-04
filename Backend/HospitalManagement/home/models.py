7b59aad44cc66dc7a0f60cd8b9fdf8ec166c897e
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2022-09-04 17:25:04 +0530

    Creating Database model

--- Backend/HospitalManagement/home/models.py
+++ Backend/HospitalManagement/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Nurse (models.Model):
+    def __init__(self: _Self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.name = models.CharField(max_length=20)
+        self.cost = 0
