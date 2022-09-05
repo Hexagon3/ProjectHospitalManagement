d3d513a0851fa36d4bf37c4924699cf74ecb644e
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2022-09-05 10:35:01 +0530

    non required

--- Backend/HospitalManagement/home/models.py
+++ Backend/HospitalManagement/home/models.py
@@ -5,18 +5,20 @@ from django.db import models
 
 class Nurse (models.Model):
     # def __init__(self, *args, **kwargs) -> None:
-    #     super().__init__(*args, **kwargs)
-    name = models.CharField(max_length=20)
-    profile_pic = models.ImageField(null=True)
+    # #     super().__init__(*args, **kwargs)
+    name = models.CharField(max_length=20, default="Nurse")
+    # profile_pic = models.ImageField(null=True)
     bio = models.TextField(null=True)
     email = models.EmailField(null=True)
     address = models.TextField(null=True)
     license = models.CharField(max_length=12, null=True)
     contact_no = models.CharField(max_length=10, null=True)
+
     cost = 0
     allocated_word = None
     shift = None
 
     def __str__(self) -> str:
+        return "nurse"
         # return super().__str__()
-        return "Nurse"+str(self.id)
+        # return "Nurse"+str(self.id)
