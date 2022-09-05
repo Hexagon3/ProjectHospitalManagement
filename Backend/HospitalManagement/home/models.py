23d0fb91163e65c7ef9a9e4ef427a522ab5d4202
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2022-09-05 10:22:19 +0530

    update ignore

--- Backend/HospitalManagement/home/models.py
+++ Backend/HospitalManagement/home/models.py
@@ -4,15 +4,19 @@ from django.db import models
 
 
 class Nurse (models.Model):
-    def __init__(self: _Self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.name = models.CharField(max_length=20)
-        self.profile_pic = models.ImageField()
-        self.bio = models.TextField()
-        self.email = models.EmailField()
-        self.address = models.TextField()
-        self.license = models.CharField(max_length=12)
-        self.contact_no = models.CharField(max_length=10)
-        self.cost = 0
-        self.allocated_word = None
-        self.shift = None
+    # def __init__(self, *args, **kwargs) -> None:
+    #     super().__init__(*args, **kwargs)
+    name = models.CharField(max_length=20)
+    profile_pic = models.ImageField(null=True)
+    bio = models.TextField(null=True)
+    email = models.EmailField(null=True)
+    address = models.TextField(null=True)
+    license = models.CharField(max_length=12, null=True)
+    contact_no = models.CharField(max_length=10, null=True)
+    cost = 0
+    allocated_word = None
+    shift = None
+
+    def __str__(self) -> str:
+        # return super().__str__()
+        return "Nurse"+str(self.id)
