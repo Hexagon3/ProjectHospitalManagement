1e7e84c50ce0b0ca52b5b39ca9556c60f51567c1
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2022-09-09 20:53:18 +0530

    nav-bug fixing

--- Frontend/statics/js/navbar.js
+++ Frontend/statics/js/navbar.js
@@ -1,6 +1,11 @@
 sidebar_active = false;
 
 
+document.getElementsByClassName("navigations-list")[0].addEventListener("resize", () => {
+    if (window.screen.width >= 1100) style.transform = 'translate(0,0)';
+})
+
+
 function open_submenu(element) {
     sub_menu = element.getElementsByClassName("sub-menu");
     x = window.matchMedia("(max-width:1022px)")
@@ -16,6 +21,7 @@ function open_submenu(element) {
 }
 
 
+
 function sidebar_open(element) {
 
     element.classList.toggle("change");
@@ -23,13 +29,16 @@ function sidebar_open(element) {
     // s[0].style["transition"] = "0.5s";
     if (sidebar_active) {
         sidebar_active = false;
-        // s[0].style.transform = 'translate(355px, -20px)'
-        s[0].style.transform = 'translate(355px, -20px)'
+        s[0].style.transform = 'translate(400px, -20px)'
+        // s[0].style.transform = 'translate(0,0)'
+
 
     }
     else {
         sidebar_active = true;
         s[0].style["transition"] = "0.5s"
         s[0].style.transform = 'translate(50px, -20px)'
+        // s[0].style.transform = 'translate(0, 0)'
     }
+
 }
