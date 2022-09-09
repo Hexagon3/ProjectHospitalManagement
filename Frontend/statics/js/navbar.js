48ef28be90e55e18b2f650525fc339a9de3b9ab9
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2022-09-09 06:52:51 +0530

    Search bar improved

--- Frontend/statics/js/navbar.js
+++ Frontend/statics/js/navbar.js
@@ -1,13 +1,12 @@
-menu_items = document.getElementsByClassName("menu-item");
-block = "block";
-inline = "inline";
+sidebar_active = false;
+
 
 function open_submenu(element) {
     sub_menu = element.getElementsByClassName("sub-menu");
     x = window.matchMedia("(max-width:1022px)")
     if (x.matches) {
         if (sub_menu[0].style["display"] == "none")
-            sub_menu[0].style["display"] = block;
+            sub_menu[0].style["display"] = 'block';
         else
             sub_menu[0].style["display"] = "none";
 
@@ -20,5 +19,16 @@ function open_submenu(element) {
 function sidebar_open(element) {
 
     element.classList.toggle("change");
-    document.getElementsByClassName("navigations-list").style.display = "block"
+    s = document.getElementsByClassName("navigations-list");
+    // s[0].style["transition"] = "0.5s";
+    if (sidebar_active) {
+        sidebar_active = false;
+        s[0].style.transform = 'translate(355px, -20px)'
+
+    }
+    else {
+        sidebar_active = true;
+        s[0].style["transition"] = "0.5s"
+        s[0].style.transform = 'translate(50px, -20px)'
+    }
 }
