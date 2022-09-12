784d465a143416d65533d38b7fc4fe30ad434020
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2022-09-12 12:33:17 +0530

    usser authentication level up

--- Backend/HospitalManagement/main/views.py
+++ Backend/HospitalManagement/main/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from urllib import request
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
 from .models import Nurse, User
 from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 
-def home(req):
-    user = {'logedin': True}
+def home(req: HttpRequest):
+    # logout(User.objects.get(username='admin'))
+
+    user = {'logedin': req.user.is_authenticated}
     context = {'user': user}
     return render(req, "home.html", context=context)
 
@@ -18,26 +21,36 @@ def signup(req):
             email = req.POST.get("email")
             passwd2 = req.POST.get('passwd2')
             passwd1 = req.POST.get('passwd1')
+
             if (passwd1 == passwd2):
                 n1 = Nurse(username=username, email=email, name=username)
                 n1.user = User.objects.create_user(
                     username=username, email=email, password=passwd2)
+                n1.save()
+                login(req, n1.user)
+                print("login Success")
+
+                user = {'logedin': req.user.is_authenticated}
+                context = {'user': user}
+                return redirect("/")
+            else:
+                context = {"message": "Login faild"}
+                return render(req, "member_login.html", context)
         except:
             email = req.POST.get("email")
             passwd = req.POST.get('password')
             nurse = Nurse.objects.get(email=email)
-            user = authenticate(nurse.username, passwd)
+            user = nurse.user  # authenticate(request)
             login(req, user)
-        #     render(req, "Signup_Page.html")
-        # new_nurse = Nurse(
-        #     name=firdt_name+' '+last_name,
-        #     email=email,
-        # )
-        # new_nurse.set_password(passwd1)
-        # new_nurse.save()
+
         return HttpResponse("Registration Success")
 
     return render(req, "member_login.html")
+
+
+def logout_session(req: HttpRequest):
+    logout(req)
+    return redirect('/')
 
 
 def nurse_profile(req):
